7f86879baa2d95911178ce486842fff4be8dcb57
Nick Gerakines <nick@gerakines.net>
2010-06-27 11:14:19 -0700

    Committed some changes

---
+++ commit.py
@@ -0,0 +1,38 @@
+import os
+import random
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+
+names = ['Nick', 'Steve', 'Andy', 'Qi', 'Fanny', 'Sarah', 'Cord', 'Todd', 'Chris', 'Pasha', 'Gabe', 'Tony', 'Jason', 'Randal', 'Ali',  'Kim', 'Rainer']
+
+def randline():
+	text = 'commit_messages.txt'
+	if not os.path.exists(text):
+		return "Shit ..."
+	f = file(text, 'rb')
+	for i,j in enumerate(f):
+		if random.randint(0,i) == i:
+			line = j
+	f.close()
+	return line
+
+def randname():
+	for i,j in enumerate(names):
+		if random.randint(0, i) == i:
+			line = j
+	return line
+
+class MainHandler(tornado.web.RequestHandler):
+	def get(self):
+		line = randline().replace("XNAMEX", randname())
+		self.render("index.html", message = line)
+
+application = tornado.web.Application([
+	(r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+	http_server = tornado.httpserver.HTTPServer(application)
+	http_server.listen(8888)
+	tornado.ioloop.IOLoop.instance().start()
