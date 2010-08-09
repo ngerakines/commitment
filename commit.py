c2167e38fda80d1891c106c9cdd34a04126a98dd
Xavier Spriet <xavier@shadow.netmon.ca>
2010-08-10 00:02:06 +0800

    Improved random selections code

--- commit.py
+++ commit.py
@@ -4,45 +4,35 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 
-names = ['Nick', 'Steve', 'Andy', 'Qi', 'Fanny', 'Sarah', 'Cord', 'Todd', 'Chris', 'Pasha', 'Gabe', 'Tony', 'Jason', 'Randal', 'Ali',  'Kim', 'Rainer']
+names = ['Nick', 'Steve', 'Andy', 'Qi', 'Fanny', 'Sarah', 'Cord', 'Todd', 
+        'Chris', 'Pasha', 'Gabe', 'Tony', 'Jason', 'Randal', 'Ali',  
+        'Kim', 'Rainer']
 
 def randline():
-	text = 'commit_messages.txt'
-	if not os.path.exists(text):
-		return "Shit ..."
-	f = file(text, 'rb')
-	for i,j in enumerate(f):
-		if random.randint(0,i) == i:
-			line = j
-	f.close()
-	return line
+    text = 'commit_messages.txt'
+    try:
+        with open(text, 'rU') as f:
+            return random.choice(f.readlines())
+    except IOError:
+        return 'Shit ...'
 
 def randname():
-	for i,j in enumerate(names):
-		if random.randint(0, i) == i:
-			line = j
-	return line
+    return random.choice(names)
 
 class MainHandler(tornado.web.RequestHandler):
-	def get(self):
-		line = randline().replace("XNAMEX", randname())
-		self.render("index.html", message = line)
-
-class PlainTextHandler(tornado.web.RequestHandler):
-	def get(self):
-		line = randline().replace("XNAMEX", randname())
-		self.write(line)
+    def get(self):
+        line = randline().replace("XNAMEX", randname())
+        self.render("index.html", message = line)
 
 settings = {
-	"static_path": os.path.join(os.path.dirname(__file__), "static"),
+    "static_path": os.path.join(os.path.dirname(__file__), "static"),
 }
 
 application = tornado.web.Application([
-	(r"/", MainHandler),
-	(r"/index.txt", PlainTextHandler),
+    (r"/", MainHandler),
 ], **settings)
 
 if __name__ == "__main__":
-	http_server = tornado.httpserver.HTTPServer(application)
-	http_server.listen(80)
-	tornado.ioloop.IOLoop.instance().start()
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(80)
+    tornado.ioloop.IOLoop.instance().start()
