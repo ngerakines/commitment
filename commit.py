5102203db366f89840181d973abccdd526f9858f
Evangelos Foutras <foutrelis@gmail.com>
2010-10-22 04:36:36 +0300

    Prevent IndexError when no arguments are provided

--- commit.py
+++ commit.py
@@ -41,7 +41,7 @@ application = tornado.web.Application([
 
 if __name__ == "__main__":
     port = 8000
-    if sys.argv[1]:
+    if len(sys.argv) > 1:
         port = int(sys.argv[1])
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(port)
